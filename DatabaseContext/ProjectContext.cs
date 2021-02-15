b1bd09e9c6bb56ec00fe95847c0459cbc4760d76
Triskae <me@filipedoutelsilva.com>
2021-02-15 17:04:22 +0100

    Added DB

---
+++ DatabaseContext/ProjectContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace projet_web.DatabaseContext
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public class Product
+        {
+            public int ProductId { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public int Price { get; set; }
+            public string Category { get; set; }
+            public List<string> Images { get; set; }
+
+            public int? OrderId { get; set; }
+            public Order? Order { get; set; }
+            
+            public int? CartId { get; set; }
+            public Cart? Cart { get; set; }
+        }
+
+        public class Order
+        {
+            public int OrderId { get; set; }
+            public DateTime OrderPlacedDate { get; set; }
+            public bool IsPayed { get; set; }
+
+            public ICollection<Product> Products { get; set; }
+
+            public int UserId { get; set; }
+            public User USer { get; set; }
+        }
+
+        public class User
+        {
+            public int UserId { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Adress { get; set; }
+            public string PostalCode { get; set; }
+            public string City { get; set; }
+            public string Avatar { get; set; }
+
+            public ICollection<Order> Orders { get; set; }
+
+            public int CartId { get; set; }
+            public Cart Cart { get; set; }
+        }
+
+        public class Cart
+        {
+            public ICollection<Product> Products { get; set; }
+            
+            public int UserId { get; set; }
+            public User User { get; set; }
+        }
+    }
+}
--- GetAllProducts.cs
+++ GetAllProducts.cs
@@ -1,22 +1,29 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using projet_web.DatabaseContext;
 
-namespace Company.Function
+namespace projet_web
 {
-    public static class getAllProducts
+    public static class GetAllProducts
     {
         [FunctionName("getAllProducts")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+            var options = new DbContextOptionsBuilder<ProjectContext>();
+            options.UseCosmos("https://administrator.documents.azure.com:443/", "ChGvETWDYQNic5koOjbfDvOs1dDXSe1VPOwftlWDSvxEIf4DQuyGApjv1QWIMlSCfWrXfy3qDHyd9is0jWSWVg==", "administrator");
+
+            var db = new ProjectContext(options.Options);
+            
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
