2899babfdec78f2e97a298f54639e56633c817d8
Sörel NADAUD <sorel.nadaud@outlook.com>
2021-03-19 23:24:11 +0100

    GetAllProducts with filter

--- Functions/Product/GetAllProducts.cs
+++ Functions/Product/GetAllProducts.cs
@@ -1,10 +1,15 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using ProjetWeb.Auth;
 using ProjetWeb.Models;
 
@@ -17,11 +22,41 @@ namespace ProjetWeb.Functions.Product
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
             HttpRequest req,
             [CosmosDB("ProjetWeb", "Products", ConnectionStringSetting = "CosmosDB")]
-            IEnumerable<Models.Product> products,
+            DocumentClient client,
             ILogger log
         )
         {
-            return new OkObjectResult(new Response<IEnumerable<Models.Product>>(products));
+            var collectionUri = UriFactory.CreateDocumentCollectionUri("ProjetWeb", "Products");
+            IDocumentQuery<Models.Product> query;
+
+            if (req.Method == HttpMethods.Post)
+            {
+                var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                var filter = JsonConvert.DeserializeObject<ProductFilter>(requestBody);
+
+                var queryOptions = new FeedOptions {EnableCrossPartitionQuery = true};
+                query = client.CreateDocumentQuery<Models.Product>(collectionUri, queryOptions)
+                    .Where(
+                        p => filter == null ||
+                             (string.IsNullOrEmpty(filter.Keyword) || p.Title.ToLower()
+                                  .Contains(string.IsNullOrEmpty(filter.Keyword)
+                                      ? string.Empty
+                                      : filter.Keyword.ToLower()) ||
+                              p.Description.ToLower().Contains(string.IsNullOrEmpty(filter.Keyword)
+                                  ? string.Empty
+                                  : filter.Keyword.ToLower())) &&
+                             (filter.LowerPriceLimit == 0 || p.Price >= filter.LowerPriceLimit) &&
+                             (filter.UpperPriceLimit == 0 || p.Price <= filter.LowerPriceLimit)
+                    )
+                    .AsDocumentQuery();
+            }
+            else
+            {
+                query = client.CreateDocumentQuery<Models.Product>(collectionUri).AsDocumentQuery();
+            }
+
+            return new OkObjectResult(
+                new Response<List<Models.Product>>(query.ExecuteNextAsync<Models.Product>().Result.ToList()));
         }
     }
 }
---
+++ Models/ProductFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjetWeb.Models
+{
+    public class ProductFilter
+    {
+        public string Keyword { get; set; }
+        public int LowerPriceLimit { get; set; }
+        public int UpperPriceLimit { get; set; }
+    }
+}
