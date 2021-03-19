9d72ac0ba0832d4c87b884d7ba1f943b95eab4b1
Sörel NADAUD <sorel.nadaud@outlook.com>
2021-03-19 21:40:50 +0100

    Modified Product model

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace ProjetWeb.Models
 {
     public class Product
     {
+        [JsonProperty("id")]
         public string id { get; set; }
+        [JsonProperty("title")]
         public string Title { get; set; }
+        [JsonProperty("description")]
         public string Description { get; set; }
+        [JsonProperty("price")]
         public int Price { get; set; }
+        [JsonProperty("year")]
         public string Year { get; set; }
-
+        [JsonProperty("horsepower")]
         public int HorsePower { get; set; }
+        [JsonProperty("mileage")]
         public int Mileage { get; set; }
+        [JsonProperty("fuel")]
         public string Fuel { get; set; }
+        [JsonProperty("images")]
         public List<string> Images { get; set; }
 
         public int? OrderId { get; set; }
